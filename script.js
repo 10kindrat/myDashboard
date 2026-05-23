e6fc3bfe3e84c8eb79c3aa6a4fe321d1b9bfb2be
10kindrat <kindrat1024@gmail.com>
2026-05-23 23:48:52 +0900

    make todo localstorage

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let todoList = JSON.parse(localStorage.getItem("todoList") || "[]");
+
+for (let i = 0; i < todoList.length; i++) {
+    let todos = document.getElementById("todo-list");
+    let li = document.createElement("li");
+    li.textContent = todoList[i];
+    todos.appendChild(li);
+    let emptyTodo = document.getElementById("empty_todo");
+    if (emptyTodo) {
+        emptyTodo.remove();
+    }
+}
+
 function clicked() {
     let value = prompt("할일을 입력하세요:");
-    if (value === null || value.trim() === "") {
-        return; // 입력이 취소되거나 빈 문자열인 경우, 함수를 종료합니다.
-    }
+    if(!value) return;
 
     // 기존의 "할 일을 추가하세요" 메시지 제거
     let emptyTodo = document.getElementById("empty_todo");
@@ -10,8 +21,12 @@ function clicked() {
         emptyTodo.remove();
     }
 
-    let todoList = document.getElementById("todo-list");
+    todoList.push(value);
+
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+
+    let todos = document.getElementById("todo-list");
     let li = document.createElement("li");
     li.textContent = value;
-    todoList.appendChild(li);
-}   
+    todos.appendChild(li);
+} 
